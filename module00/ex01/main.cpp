b515691e6ee9053ce6ccc77cb0ae448aee3cd524
k010103 <k010103@naver.com>
2021-07-25 03:16:00 +0900

    phone_book Modifying

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -1,18 +1,45 @@
 #include "phone_book.hpp"
 
-int                 main(void)
+void				explanation(void)
 {
-    std::string     input;
-    // contact_info;
+	std::cout << "you should accept the ADD command, the SEARCH command or the EXIT command." << "\n";
+	std::cout << "EXIT : program quits." << "\n";
+	std::cout << "ADD : new contact’s information." << "\n";
+	std::cout << "SEARCH : SEARCH phone_book." << "\n";
+}
 
-    while (true)
-    {
-        std::getline(std::cin, input);
-        if (input == "exit")
-            break ;
-        std::cout << input << "\n";
-    }
-    
-    std::cout << "good bye :D" << "\n";
-    return (0);
+int					main(void)
+{
+	std::string		input;
+	phone_book		phone_book[8];
+	int				count = 0;
+
+	explanation();
+	while (true)
+	{
+		std::cout << "📖 phone_book : ";
+		std::getline(std::cin, input);
+		if (input == "EXIT")
+			break ;
+		else if (input == "ADD")
+		{
+			phone_book[count].ADD();
+			std::cout << "It's been saved in number " << count + 1 << "\n";
+			count++;
+		}
+		else if (input == "SEARCH")
+		{
+			for (int i = 0; i < 8; i++)
+				phone_book[i].SEARCH();
+		}
+		else
+		{
+			std::cout << "Invalid input value. (EXIT, ADD, SEARCH)" << "\n";
+		}
+		if (count >= 8)
+			count = 0;
+	}
+	
+	std::cout << "good bye :D" << "\n";
+	return (0);
 }
--- module00/ex01/phone_book.hpp
+++ module00/ex01/phone_book.hpp
@@ -1,8 +1,61 @@
-#   ifndef PHONE_BOOK_HPP
-#   define PHONE_BOOK_HPP
+#	ifndef PHONE_BOOK_HPP
+#	define PHONE_BOOK_HPP
 
-// include
+// include ============================
 
-#   include <iostream>
+#	include <iostream>
+#	include <string>
+
+// class ==============================
+
+class phone_book
+{
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
+public:
+	void			ADD(void)
+	{
+		std::string	temp;
+
+		std::cout << "first_name : ";
+		getline(std::cin, temp);
+		first_name = temp;
+		std::cout << "last_name : ";
+		getline(std::cin, temp);
+		first_name = temp;
+		std::cout << "nickname : ";
+		getline(std::cin, temp);
+		first_name = temp;
+		std::cout << "phone_number : ";
+		getline(std::cin, temp);
+		first_name = temp;
+		std::cout << "darkest_secret : ";
+		getline(std::cin, temp);
+		first_name = temp;
+		std::cout << "\n";
+	}
+	// [  10보다  ]|[  작은      ]|[   출력     ]|[          ]
+	// index, first name, last name and nickname
+	void SEARCH(void)
+	{
+
+	}
+	phone_book(/* args */);
+	~phone_book();
+};
+
+phone_book::phone_book(/* args */)
+{
+}
+
+phone_book::~phone_book()
+{
+}
+
+// class ==============================
 
 #endif
