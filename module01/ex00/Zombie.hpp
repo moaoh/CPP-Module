df4c51539bc0c1fbda6de6906b33bdc79b17e113
k010103 <k010103@naver.com>
2021-07-27 00:27:44 +0900

    zombie Modifying

---
+++ module01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+#	ifndef ZOMBIE_HPP
+#	define ZOMBIE_HPP
+
+// icnlude ============================
+
+#	include <iostream>
+
+// class ==============================
+
+class Zombie
+{
+private:
+	/* data */
+public:
+	Zombie(/* args */);
+	~Zombie();
+};
+
+Zombie::Zombie(/* args */)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+// ====================================
+
+#	endif
