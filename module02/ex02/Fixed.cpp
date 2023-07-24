3d8644b28f327596103a160bbca50d87a0481a02
moaoh <k010103@naver.com>
2023-07-24 23:23:52 +0900

    feat: :sparkles: module02 - ex02 해결!!

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,200 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	this->value = 0;
+}
+
+Fixed::Fixed( const Fixed& fixed )
+{
+	*this = fixed;
+}
+
+Fixed::Fixed( const int raw )
+{
+	this->value = (raw << integer);
+}
+
+Fixed::Fixed( const float raw )
+{
+	this->value = (int)roundf((raw * (1 << integer)));
+}
+
+int 	Fixed::getRawBits( void ) const
+{
+	return (this->value);
+}
+
+void 	Fixed::setRawBits( int const raw )
+{
+	this->value = raw;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return ((float)this->value / (1 << integer));
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (this->value >> integer);
+}
+
+Fixed 	&Fixed::operator = ( const Fixed& fixed )
+{
+	this->value = fixed.getRawBits();
+	return (*this);
+}
+
+// ========== comparison operators ==========
+
+bool	Fixed::operator > ( const Fixed& fixed ) const
+{
+	if (this->value > fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+bool	Fixed::operator < ( const Fixed& fixed ) const
+{
+	if (this->value < fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+bool	Fixed::operator >= ( const Fixed& fixed ) const
+{
+	if (this->value >= fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+bool	Fixed::operator <= ( const Fixed& fixed ) const
+{
+	if (this->value <= fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+bool	Fixed::operator == ( const Fixed& fixed ) const
+{
+	if (this->value == fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+bool	Fixed::operator != ( const Fixed& fixed ) const
+{
+	if (this->value != fixed.value)
+		return (1);
+	else
+		return (0);
+}
+
+// ========== arithmetic operators ==========
+
+Fixed	Fixed::operator + ( const Fixed& fixed ) const
+{
+	Fixed temp = (this->value + fixed.value) >> integer;
+	// std::cout << temp.value << "\n";
+	return (temp);
+}
+
+Fixed	Fixed::operator - ( const Fixed& fixed ) const
+{
+	Fixed temp = (this->value - fixed.value) >> integer;
+	return (temp);
+}
+
+Fixed	Fixed::operator * ( const Fixed& fixed ) const
+{
+	float first = (float)this->value / (1 << integer);
+	float second = (fixed.value >> integer);
+	Fixed temp = first * second;
+	return (temp);
+}
+
+Fixed	Fixed::operator / ( const Fixed& fixed ) const
+{
+	float first = (float)this->value / (1 << integer);
+	float second = (fixed.value >> integer);
+	Fixed temp = first / second;
+	return (temp);
+}
+
+// ========== increment/decrement ==========
+
+Fixed	&Fixed::operator++()
+{
+	this->value++;
+	return (*this);
+}
+
+Fixed	&Fixed::operator--()
+{
+	this->value--;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed temp = *this;
+	++*this;
+	return (temp);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed temp = *this;
+	--*this;
+	return (temp);
+}
+
+Fixed	Fixed::max( Fixed& first, Fixed& second )
+{
+	if (first.value > second.value) 
+		return first.toFloat();
+	else 
+		return second.toFloat();
+}
+
+Fixed	Fixed::max( const Fixed& first, const Fixed& second )
+{
+	if (first.value > second.value) 
+		return first.toFloat();
+	else 
+		return second.toFloat();
+}
+
+Fixed	Fixed::min( Fixed& first, Fixed& second )
+{
+	if (first.value < second.value) 
+		return first.toFloat();
+	else 
+		return second.toFloat();
+}
+
+Fixed	Fixed::min( const Fixed& first, const Fixed& second )
+{
+	if (first.value < second.value) 
+		return first.toFloat();
+	else 
+		return second.toFloat();
+}
+
+// ===============   ===============
+
+
+Fixed::~Fixed()
+{
+}
+
+std::ostream& operator << (std::ostream& os, const Fixed& fixed)
+{
+	return os << fixed.toFloat();
+}
---
+++ module02/ex02/Fixed.hpp
@@ -0,0 +1,53 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int					value;
+	static const int	integer = 8;
+public:
+	Fixed();
+	Fixed( const Fixed& fixed );
+	Fixed( const int raw );
+	Fixed( const float raw );
+	
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+	Fixed &operator = ( const Fixed& fixed );
+
+	bool	operator > ( const Fixed& fixed ) const;
+	bool	operator < ( const Fixed& fixed ) const;
+	bool	operator >= ( const Fixed& fixed ) const;
+	bool	operator <= ( const Fixed& fixed ) const;
+	bool	operator == ( const Fixed& fixed ) const;
+	bool	operator != ( const Fixed& fixed ) const;
+
+	Fixed	operator + ( const Fixed& fixed ) const;
+	Fixed	operator - ( const Fixed& fixed ) const;
+	Fixed	operator * ( const Fixed& fixed ) const;
+	Fixed	operator / ( const Fixed& fixed ) const;
+
+	Fixed	&operator++();
+	Fixed	&operator--();
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+
+	static Fixed	max( Fixed& first, Fixed& second );
+	static Fixed	max( const Fixed& first, const Fixed& second );
+	static Fixed	min( Fixed& first, Fixed& second );
+	static Fixed	min( const Fixed& first, const Fixed& second );
+
+	~Fixed();
+};
+
+std::ostream &operator << (std::ostream& os, const Fixed& fixed);
+
+#endif
