999840357969d654ce99a62f17d05941a8cb0aaa
moaoh <k010103@naver.com>
2023-10-19 17:57:30 +0900

    style: :art: ex02 - main안에서 선언된 변수이름 변경

--- module05/ex02/src/main.cpp
+++ module05/ex02/src/main.cpp
@@ -6,18 +6,28 @@
 
 int	main() {
 	try {
-		Bureaucrat	a("a", 150);
-		Bureaucrat	b("b", 1);
+		Bureaucrat	superBureaucrat("superBureaucrat", 1);
+		Bureaucrat	weakBureaucrat("weakBureaucrat", 150);
 
 		PresidentialPardonForm	form1("target1");
 		RobotomyRequestForm			form2("target2");
 		ShrubberyCreationForm		form3("target3");
 
-		a.signForm(form3);
-		a.executeForm(form3);
+		// error case ===
+		weakBureaucrat.signForm(form3);
+		weakBureaucrat.executeForm(form3);
 
-		b.signForm(form3);
-		b.executeForm(form3);
+		superBureaucrat.executeForm(form1);
+		/// =============
+
+		superBureaucrat.signForm(form1);
+		superBureaucrat.executeForm(form1);
+
+		superBureaucrat.signForm(form2);
+		superBureaucrat.executeForm(form2);
+
+		superBureaucrat.signForm(form3);
+		superBureaucrat.executeForm(form3);
 	}
 	catch (const std::exception &e) {
 		std::cout << "error : " << e.what() << std::endl;
