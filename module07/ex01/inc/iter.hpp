df4cc3d4c767662859191edbf6dbd8fb9722c9ec
moaoh <k010103@naver.com>
2024-01-09 20:51:12 +0900

    feat: :zap: add cpp07 - ex01 iter.cpp code

--- module07/ex01/inc/iter.hpp
+++ module07/ex01/inc/iter.hpp
@@ -4,6 +4,17 @@
 #include <iostream>
 
 template <typename T>
-void iter(T address, T length, T);
+void  iter(T *address, size_t len, void(*fun)(T& t)) {
+  for (size_t i=0; i < len ; i++) {
+		fun(address[i]);
+	}
+}
+
+template <typename T>
+void  iter(T *address, size_t len, void(*fun)(T const& t)) {
+  for (size_t i=0; i < len ; i++) {
+		fun(address[i]);
+	}
+}
 
 #endif
