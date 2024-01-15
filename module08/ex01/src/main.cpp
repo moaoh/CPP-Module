419851c9fd68832566509f040e6ae43590d7a326
moaoh <k010103@naver.com>
2024-01-15 14:33:14 +0900

    fix: :bug: cpp08 - ex01 add exception handling

--- module08/ex01/src/main.cpp
+++ module08/ex01/src/main.cpp
@@ -51,9 +51,16 @@ int main()
 		for (int i = 1; i <= 10000; i++) {
 			vec.push_back(i);
 		}
-		sp.addNumber(vec.begin(), vec.end());
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+		try {
+			sp.addNumber(vec.begin(), vec.end());
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+
+		}
+		catch(const std::exception& e) {
+			std::cerr << e.what() << '\n';
+		}
+
 	}
 
 	return 0;
