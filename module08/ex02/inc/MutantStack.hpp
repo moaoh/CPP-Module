1f20bdb37e68215989ce82096fc5549f99200c29
moaoh <k010103@naver.com>
2024-01-15 17:17:33 +0900

    feat: :sparkles: cpp08 - ex02 is done.

---
+++ module08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,38 @@
+#ifndef MutantStack_hpp_
+#define MutantStack_hpp_
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+public:
+	MutantStack() {};
+	MutantStack(const MutantStack& src) { *this = src; }
+	MutantStack &operator=(const MutantStack& rhs) {
+		*this = rhs;
+		return *this;
+	}
+	~MutantStack() {};
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+  typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+  typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+
+  const_iterator begin(void) const { return this->c.begin(); }
+  const_iterator end(void) const { return this->c.end(); }
+
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+
+	const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+	const_reverse_iterator rend() const { return this->c.rend(); }
+
+};
+
+#endif
