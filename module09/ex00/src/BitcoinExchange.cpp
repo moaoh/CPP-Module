c085f712efb14b2b2e7efc23a03049b1b6fab958
moaoh <k010103@naver.com>
2024-01-18 22:55:23 +0900

    feat: :construction: cpp09 - ex00 working..

--- module09/ex00/src/BitcoinExchange.cpp
+++ module09/ex00/src/BitcoinExchange.cpp
@@ -39,22 +39,6 @@ static bool hasExtension(const char* fileName, const char* extension) {
 	return std::strcmp(fileExtension, extension) == 0;
 }
 
-// 양수만 가능.
-// Error: not a positive number
-// int 범위를 넘는경우 Cut!
-// Error: too large a number.
-// 입력 값이 잘못된 경우 Cut!
-// Error: bad input => 2001-42-42
-static bool checkValidInputLine(double value) {
-	if (value < 0) {
-		throw std::runtime_error("not a positive number");
-	}
-	if (2147483647 < value || value < -2147483647) {
-		throw std::runtime_error("too large a number.");
-	}
-	return true;
-}
-
 void	BitcoinExchange::inBitcoinData() {
 	std::ifstream outFile("data.csv");
 	if (!outFile.is_open()) {
@@ -73,6 +57,63 @@ void	BitcoinExchange::inBitcoinData() {
 	}
 }
 
+static bool isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+static bool isValidDate(std::string key) {
+	if (countOccurrences(key, '-') != 2) {
+		throw std::runtime_error("bad input => " + key);
+	}
+	int firstTarget = key.find("-");
+	int secondTarget = key.rfind("-");
+	int year = std::atoi(key.substr(0, firstTarget).c_str());
+	int month = std::atoi(key.substr(firstTarget + 1, secondTarget).c_str());
+	int day = std::atoi(key.substr(secondTarget + 1).c_str());
+	const int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	if (year < 1000 || 10000 <= year || month < 1 || 12 < month || day < 1) {
+		throw std::runtime_error("bad input => " + key);
+	}
+	if (month == 2 && isLeapYear(year)) {
+		if (day < 1 || 29 < day) {
+			throw std::runtime_error("bad input => " + key);
+		}
+		return true;
+	}
+	else {
+		if (day < 1 || daysInMonth[month] < day) {
+			throw std::runtime_error("bad input => " + key);
+		}
+		return true;
+	}
+}
+
+// 양수만 가능.
+// Error: not a positive number
+// int 범위를 넘는경우 Cut!
+// Error: too large a number.
+// 입력 값이 잘못된 경우 Cut!
+// Error: bad input => 2001-42-42
+// time_value 
+static bool checkValidInputLine(std::string line, std::string &key, double &value) {
+	// TODO : date | value 형식따로 반례처리 필요.
+	std::size_t pos = line.find("|");
+	if (pos == std::string::npos || countOccurrences(line, '|') != 1) {
+		throw std::runtime_error("bad input => " + key);
+	}
+	char* endPtr;
+	key = line.substr(0, pos);
+	value = strtod(line.substr(pos + 1).c_str(), &endPtr);
+	if (value < 0) {
+		throw std::runtime_error("not a positive number");
+	}
+	if (2147483647 < value) {
+		throw std::runtime_error("too large a number.");
+	}
+	isValidDate(key);
+	return true;
+}
+
 // 확장자가 .txt인지 확인 필요. 
 void BitcoinExchange::runInput(std::string fileName) const {
 	std::ifstream outFile(fileName);
@@ -85,21 +126,25 @@ void BitcoinExchange::runInput(std::string fileName) const {
 	std::string line;
 	while (std::getline(outFile, line)) {
 		try {
-			std::size_t pos = line.find("|");
-			if (pos == std::string::npos || countOccurrences(line, '|') != 1) {
-				throw std::runtime_error("bad input => " + line);
-			}
-			char* endPtr;
-			std::string key = line.substr(0, pos);
-			double value = strtod(line.substr(pos + 1).c_str(), &endPtr);
-			checkValidInputLine(value);
 			std::map<std::string, double>::const_iterator it;
+			std::string key;
+			double value, res;
+
+			checkValidInputLine(line, key, value);
 			it = this->_bitcoinData.lower_bound(key);
-			if (it == this->_bitcoinData.begin() || it == this->_bitcoinData.end()) {
+			if (it == this->_bitcoinData.begin()) {
 				throw std::runtime_error("invalid value");
 			}
 			--it;
-			std::cout << key << "=> " << value << " = " << value * it->second << std::endl;
+			res = value * (it->second);
+			std::cout << "value :" << value << std::endl;
+			std::cout << "it->second :" << it->second << std::endl;
+			std::cout << std::fixed << "res :" << res << std::endl;
+			// TODO : 소수점 정리필요.
+			if (2147483647 < res) {
+				throw std::runtime_error("over value | max : 2147483647");
+			}
+			std::cout << std::fixed << key << "=> " << value << " = " << res << std::endl;
 		}
 		catch (const std::exception& e) {
 			std::cerr << "Error: " << e.what() << '\n';
